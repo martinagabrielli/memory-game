4d521ad933bcb6ac2858245b4c26c7e66bba16e3
Martina Gabrielli <martina.gabrielli@yahoo.it>
2020-03-03 14:39:31 +0000

    added cards data

---
+++ script.js
@@ -0,0 +1,52 @@
+// Card data
+
+const cardsArray = [
+    {
+        name: 'ammo',
+        img: 'img/ammo-chest.png',
+    },
+    {
+        name: 'banana',
+        img: 'img/banana.png',
+    },
+    {
+        name: 'bandages',
+        img: 'img/bandages.png',
+    },
+    {
+        name: 'bus',
+        img: 'img/bus.png',
+    },
+    {
+        name: 'cuddle',
+        img: 'img/cuddle.png',
+    },
+    {
+        name: 'ginger',
+        img: 'img/ginger-gunner.png',
+    },
+    {
+        name: 'harpoon',
+        img: 'img/harpoon.png',
+    },
+    {
+        name: 'llama',
+        img: 'img/llama.png',
+    },
+    {
+        name: 'pickaxe',
+        img: 'img/pickaxe.png',
+    },
+    {
+        name: 'potion',
+        img: 'img/potion-shield.png',
+    },
+    {
+        name: 'rainbow',
+        img: 'img/rainbow-pickaxe.png',
+    },
+    {
+        name: 'treasure',
+        img: 'img/treasure-chest.png',
+    },
+]
